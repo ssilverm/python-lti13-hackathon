70dabf9dedaf23b629a12fcd643166de6a967e66
Shea Silverman <shea.silverman@ucf.edu>
2018-05-23 16:17:18 -0400

    IMS Global LILI18 LTI Advantage Hackathon

--- views.py
+++ views.py
@@ -69,6 +69,91 @@ def index(lti=lti):
     return render_template('index.htm.j2')
 
 
+#JWT test
+@app.route('/jwt_launch_via_json', methods=['POST', 'GET'])
+def jwt_launch_via_json():
+    from jose import jwk, jws, jwt
+    from jose.utils import base64url_decode
+    import requests
+
+    #from keys import public_key, private_key
+    print("=-=-=-=-=-=- REQUEST FORM -=-=-=-=-=")
+    print(json.dumps(request.form, indent=2))
+    
+    #jwt
+    id_token = request.form.get('id_token')
+    audience='407321823'
+
+    #key id
+    kid = jws.get_unverified_header(id_token)['kid']
+
+    #get platform web keys from lti-ri
+    r = requests.get(settings.JSON_KEY_URL)
+    
+    #todo - get key based on KID
+    web_keys = r.json()['keys'][0]
+
+    #create key from web keys
+    key = jwk.construct(web_keys)
+
+
+    try:
+        #Verify JWT
+        lti_data = jwt.decode(id_token, web_keys, algorithms=['RS256'], audience=audience)
+
+        print("=-=-=-=-=-=- LTI DATA -=-=-=-=-=")
+        print(lti_data)
+
+        return "Hello %s! <br> JWT Successfully Decoded and verified." % lti_data['name']
+
+    except:  
+        return "There was an error verifying the JWT."
+
+
+    return "Should not get here"
+
+
+
+
+
+# #JWT test
+@app.route('/jwt_launch', methods=['POST', 'GET'])
+def jwt_launch():
+    #return render_template('index.htm.j2')
+    import jwt
+    import json
+    from jose import jws
+
+    from keys import public_key, private_key
+    
+    print(json.dumps(request.form, indent=2))
+    id_token = str.encode(request.form.get('id_token'))
+    audience= settings.AUDIENCE
+
+    #python-jose
+    # try:
+    #     verified = jws.verify(id_token, public_key, algorithms=['RS256'])
+    #     return "JWT Successfully Decoded and verified."
+    # except:
+    #     return "There was an error verifying the JWT."
+    
+    #pyjwt
+    try:
+        decoded = jwt.decode(id_token, public_key, audience=audience, algorithms='RS256')
+        print(decoded)
+        return "Hello %s! <br> JWT Successfully Decoded and verified." % decoded['name']
+    except:
+        return "There was an error verifying the JWT."
+    
+    return json.dumps(decoded)
+    #return "hi"
+
+
+
+
+
+
+
 # LTI XML Configuration
 @app.route("/xml/", methods=['GET'])
 def xml():
